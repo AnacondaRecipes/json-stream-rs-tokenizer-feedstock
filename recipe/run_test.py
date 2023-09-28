0edae68fd59dc4013d7e26f7b936a0d685127db7
Lorenzo Pirritano <lpirritano@anaconda.com>
2023-09-28 18:21:40 +0200

    add rust_tokenizer_or_raise test

---
+++ recipe/run_test.py
@@ -0,0 +1,4 @@
+print("import: 'json_stream_rs_tokenizer'")
+import json_stream_rs_tokenizer
+print("json_stream_rs_tokenizer.rust_tokenizer_or_raise()")
+json_stream_rs_tokenizer.rust_tokenizer_or_raise()

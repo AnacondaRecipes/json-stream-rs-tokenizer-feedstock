29849dfc95e627cfe05a0e806fee4a38d452a0f4
Lorenzo Pirritano <lpirritano@anaconda.com>
2023-10-02 20:10:56 +0200

    address PR comments

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,4 +1,8 @@
 print("import: 'json_stream_rs_tokenizer'")
 import json_stream_rs_tokenizer
+
+# this test is very important to detect if the rust
+# package was built correctly (the rust toolchain must
+# be present)
 print("json_stream_rs_tokenizer.rust_tokenizer_or_raise()")
 json_stream_rs_tokenizer.rust_tokenizer_or_raise()
